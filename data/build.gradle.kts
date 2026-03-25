7cbc4254c745ce6e0485de72bbf6afbf5863b120
sinanali <sinanalizz123@gmail.com>
2026-03-25 08:27:20 +0530

    Align JVM target to 17

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -11,6 +11,15 @@ android {
     defaultConfig {
         minSdk = 26
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
